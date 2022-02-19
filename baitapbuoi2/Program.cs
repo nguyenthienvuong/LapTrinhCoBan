0fb153517873e4e401a75dab9d8c033977a068a6
unknown <nguyenthienvuong.it@gmail.com>
2022-02-20 06:15:42 +0700

    baitapbuoi2

---
+++ baitapbuoi2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+namespace Lesson1
+{
+    class Program
+    {
+        public static void Main(string[] arg)
+        {
+            // BÀI TẬP 1
+            Console.WriteLine("Ngon ngu lap trinh C");
+
+            // BÀI TẬP 2
+            int n;
+            n=100;
+            Console.WriteLine(n);
+            n=n+200;
+            Console.WriteLine(n);
+            
+            // BÀI TẬP 3
+            //1
+            Console.WriteLine("*");
+            Console.WriteLine("**");
+            Console.WriteLine("***");
+            Console.WriteLine("****");
+
+            for(int i=0;i<4;i++)
+            {
+                Console.Write("*");
+                // for(int j=0;j<=i;j++);
+                // {
+                // }
+            Console.WriteLine("\n"); 
+            }
+        }
+    }
+}
