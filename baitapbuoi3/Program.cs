3b2b2f3d7899daeffd531ed876dfb42925b543ec
unknown <nguyenthienvuong.it@gmail.com>
2022-02-24 14:30:37 +0700

    update baihocbuoi3 và baitapbuoi3

--- baitapbuoi3/Program.cs
+++ baitapbuoi3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Linq;
 namespace Lesson1
 {
     class Program
@@ -98,10 +99,36 @@ namespace Lesson1
 
 
              // VÍ DỤ VỀ CẤU TRÚC MẢNG, DỮ LIỆU MẢNG
-             Console.WriteLine("*** VÍ DỤ VỀ CẤU TRÚC MẢNG, DỮ LIỆU MẢNG ***");
+            Console.WriteLine("*** VÍ DỤ VỀ CẤU TRÚC MẢNG, DỮ LIỆU MẢNG ***");
 
-            string[] hangDienThoai = new string[3]{"iPhone","Samsung", "Nokia"};
-            Console.WriteLine(hangDienThoai);`
+            string[] hangDienThoai = {"iPhone","Samsung", "Nokia"};
+            ///string[] hangDienThoai = new string[3] {"iPhone","Samsung", "Nokia"};
+            Console.WriteLine($"{hangDienThoai[0]}");
+            Console.WriteLine($"{hangDienThoai[1]}");
+            Console.WriteLine($"{hangDienThoai[2]}");
+
+            // string[] hangDienThoai = {"iPhone","Samsung", "Nokia"};
+            double[] giaDienThoai = {35000000, 15000000, 10000000};
+            var giaDienThoaiiPhone = string.Format("{0:n0}",giaDienThoai[0]);
+            var giaDienThoaiSamsung = string.Format("{0:n0}",giaDienThoai[1]);
+            var giaDienThoaiNokia = string.Format("{0:n0}",giaDienThoai[2]);
+            Console.WriteLine($"{hangDienThoai[0]} có giá là {giaDienThoaiiPhone}");
+            Console.WriteLine($"{hangDienThoai[0]} có giá là {giaDienThoaiSamsung}");
+            Console.WriteLine($"{hangDienThoai[0]} có giá là {giaDienThoaiNokia}");
+            // Console.WriteLine($"{hangDienThoai[0]} có giá là {giaDienThoai[0]}");
+            // Console.WriteLine($"{hangDienThoai[1]} có giá là {giaDienThoai[1]}");
+            // Console.WriteLine($"{hangDienThoai[2]} có giá là {giaDienThoai[2]}");
+            Console.WriteLine(hangDienThoai.Length);
+            Console.WriteLine(hangDienThoai.Rank);
+            // var giaDienThoaiCopy= giaDienThoai[2].Clone();
+            // Console.WriteLine(giaDienThoaiCopy);
+            Console.WriteLine(giaDienThoai.GetValue(0));
+            Console.WriteLine(giaDienThoai.Max());
+            Console.WriteLine(giaDienThoai.Min());
+            Console.WriteLine(giaDienThoai.Sum());
+            
+            // VÍ DỤ VỀ CẤU TRÚC MẢNG, DỮ LIỆU MẢNG
+            Console.WriteLine("*** VÍ DỤ VỀ CẤU TRÚC MẢNG, DỮ LIỆU MẢNG ***");
         }
     }
 }
---
+++ nhap/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+namespace Lesson1
+{
+    class Program
+    {
+        public static void Main(string[] arg)
+        {
+            Console.InputEncoding = Encoding.UTF8; 
+            Console.OutputEncoding = Encoding.UTF8; 
+
+            
+            string[] hangDienThoai = {"iPhone","Samsung", "Nokia"};
+            double[] giaDienThoai = {35000000, 15000000, 10000000};
+            var giaDienThoaiiPhone = string.Format("{0:n0}",giaDienThoai[0]);
+            var giaDienThoaiSamsung = string.Format("{0:n0}",giaDienThoai[1]);
+            var giaDienThoaiNokia = string.Format("{0:n0}",giaDienThoai[2]);
+            Console.WriteLine($"{hangDienThoai[0]} có giá là {giaDienThoaiiPhone}");
+            Console.WriteLine($"{hangDienThoai[0]} có giá là {giaDienThoaiSamsung}");
+            Console.WriteLine($"{hangDienThoai[0]} có giá là {giaDienThoaiNokia}");
+            // Console.WriteLine($"{hangDienThoai[0]} có giá là {giaDienThoai[0]}");
+            // Console.WriteLine($"{hangDienThoai[1]} có giá là {giaDienThoai[1]}");
+            // Console.WriteLine($"{hangDienThoai[2]} có giá là {giaDienThoai[2]}");
+            Console.WriteLine(hangDienThoai.Length);
+            Console.WriteLine(hangDienThoai.Rank);
+            // var giaDienThoaiCopy= giaDienThoai[2].Clone();
+            // Console.WriteLine(giaDienThoaiCopy);
+            Console.WriteLine(giaDienThoai.GetValue(0));
+            Console.WriteLine(giaDienThoai.Max());
+            Console.WriteLine(giaDienThoai.Min());
+            Console.WriteLine(giaDienThoai.Sum());
+
+        }
+    }
+}
