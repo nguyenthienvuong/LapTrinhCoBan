6fe7b3f583b809a8cd18b8b2bd0d8388500f72d2
unknown <nguyenthienvuong.it@gmail.com>
2022-03-24 16:25:29 +0700

    Bai tap array

--- ARRAY/BT3.cs
+++ ARRAY/BT3.cs
@@ -8,7 +8,24 @@ namespace Array
         {
             Console.InputEncoding = Encoding.UTF8;
             Console.OutputEncoding = Encoding.UTF8;
-            
+
+            //Mảng hai chiều
+            int M,N;
+            Console.Write("Please enter size of column of 2-way array : ");
+            M = int.Parse(Console.ReadLine());
+            Console.Write("Please enter size of row of 2-way array : ");
+            N = int.Parse(Console.ReadLine());
+            float[,] array2 = new float[M,N];
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write($" Please enter the array2[{i}, {j}] element : ");
+                    array2[i, j] = float.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine(array2);
+
         }
     }
 }
---
+++ LOOP/BT1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text;
+namespace Loop
+{
+    class BT1
+    {
+        public static void Main(string[] arg)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Hello");
+
+        }
+    }
+}
